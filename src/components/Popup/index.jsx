35246a5db8221ac2b5809128d9e08952105012e1
yucel.polat <yclplt@gmail.com>
2023-09-03 18:52:09 +0300

    formatted popup

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -5,28 +5,29 @@ import { useData } from '../../DataContext';
 const Popup = () => {
     const { betData } = useData();
 
-    if (!betData?.length) return
+    if (!betData?.length) return null;
 
-    const multipliedResult = betData.reduce((accumulator, currentValue) => {
-        return accumulator * currentValue.cellData;
-    }, 1);
+    const multipliedResult = betData.reduce(
+        (accumulator, currentValue) => accumulator * currentValue.cellData,
+        1
+    );
 
     const formattedResult = multipliedResult.toFixed(2);
 
     return (
         <div className="popup">
             <div className="popup-content">
-                {betData?.map((bet, index) =>
-                    <div className='popup-row' key={`${bet?.data?.C}${index}`}>
-                        <div>{bet?.data?.OCG?.[1]?.MBS} Kod: {bet?.data?.C}</div>
-                        <div className='content'>Maç: {bet?.data?.N}</div>
+                {betData.map((bet, index) => (
+                    <div className="popup-row" key={`${bet?.data?.C}${index}`}>
+                        <div>
+                            {bet?.data?.OCG?.[1]?.MBS} Kod: {bet?.data?.C}
+                        </div>
+                        <div className="content">Maç: {bet?.data?.N}</div>
                         <div>Oran: {bet?.cellData}</div>
                     </div>
-                )}
+                ))}
             </div>
-            <div className='popup-total'>
-                Toplam: {formattedResult}
-            </div>
+            <div className="popup-total">Toplam: {formattedResult}</div>
         </div>
     );
 };
