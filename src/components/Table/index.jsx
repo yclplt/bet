71ded303ca1ae94b618614b81fa67f3f7060978a
yucel.polat <yclplt@gmail.com>
2023-09-03 13:06:33 +0300

    Using UseReducer solved code complexity and improved performance

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,39 +1,84 @@
-import React, { useMemo, useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { FixedSizeList as List } from 'react-window';
 
+const initialState = {
+    selectedCells: [],
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'TOGGLE_CELL_SELECTION':
+            const { rowIndex, columnIndex, data, cellData } = action.payload;
+            const clickedCell = { rowIndex, columnIndex, data, cellData };
+
+            const isSelected = state.selectedCells.some(
+                (cell) =>
+                    cell.rowIndex === clickedCell.rowIndex &&
+                    cell.columnIndex === clickedCell.columnIndex
+            );
+
+            const updatedSelectedCells = state.selectedCells.filter(
+                (cell) => cell.rowIndex !== clickedCell.rowIndex
+            );
+
+            if (!isSelected) {
+                updatedSelectedCells.push(clickedCell);
+            }
+
+            return { ...state, selectedCells: updatedSelectedCells };
+
+        case 'CLEAR_SELECTION':
+            return { ...state, selectedCells: [] };
+
+        default:
+            return state;
+    }
+}
+
 const Table = ({ columns, data = [], onSelect }) => {
-    const [selectedCells, setSelectedCells] = useState([]); 
+    const [state, dispatch] = useReducer(reducer, initialState);
     const rowHeight = 40;
 
+    useEffect(() => {
+        onSelect(state.selectedCells);
+    }, [state.selectedCells, onSelect]);
+
     const getHeaders = () => {
-        const headerData = columns?.map((column) => (
-            <div key={column?.accessorKey} className="header-cell" style={{ minWidth: column?.width ?? "max-width" }}>
+        return columns.map((column) => (
+            <div
+                key={column?.accessorKey}
+                className="header-cell"
+                style={{ minWidth: column?.width ?? 'max-width' }}
+            >
                 {column?.header}
             </div>
-        ))
-        return headerData
-    }
-
-    const handleCellClick = (index, columnIndex) => {
-        const clickedCell = { rowIndex: index, columnIndex };
-
-        const isSelected = selectedCells?.some((cell) => (
-            cell?.rowIndex === clickedCell?.rowIndex && cell?.columnIndex === clickedCell?.columnIndex
         ));
+    };
 
-        const updatedSelectedCells = selectedCells?.filter((cell) => cell?.rowIndex !== clickedCell?.rowIndex);
+    const handleCellClick = (index, columnIndex, cellData) => {
+        const isHeader = index % 2 === 0;
+        const isClickable = !isHeader && columns?.[columnIndex]?.clickable;
 
-        if (!isSelected) {
-            updatedSelectedCells.push(clickedCell);
+        if (isClickable) {
+            dispatch({
+                type: 'TOGGLE_CELL_SELECTION',
+                payload: {
+                    rowIndex: index,
+                    columnIndex,
+                    data: data?.[index],
+                    cellData,
+                },
+                onSelect
+            });
         }
 
-        setSelectedCells(updatedSelectedCells);
-        onSelect(updatedSelectedCells);
     };
 
     const getCellClassName = (index, columnIndex) => {
         const isHeader = index % 2 === 0;
-        const isSelected = selectedCells?.some((cell) => cell?.rowIndex === index && cell?.columnIndex === columnIndex);
+        const isSelected = state.selectedCells.some(
+            (cell) => cell?.rowIndex === index && cell?.columnIndex === columnIndex
+        );
         const isClickable = !isHeader && columns?.[columnIndex]?.clickable;
 
         const cellClassNames = ['table-cell'];
@@ -49,44 +94,37 @@ const Table = ({ columns, data = [], onSelect }) => {
         return cellClassNames.join(' ');
     };
 
-    const handleCellClickWrapper = (index, columnIndex) => {
-        const isHeader = index % 2 === 0;
-        const isClickable = !isHeader && columns?.[columnIndex]?.clickable;
-
-        if (isClickable) {
-            handleCellClick(index, columnIndex);
+    const renderCellValue = (column, index) => {
+        if (index % 2 === 1) {
+            return column?.renderValue
+                ? column.renderValue({ row: data?.[index] })
+                : data?.[index]?.[column];
+        } else {
+            return column?.headerValue
+                ? column.headerValue({ row: data?.[index] })
+                : column?.header;
         }
     };
 
     return (
         <div className="table-container">
-            <div className="header-row">
-                {getHeaders()}
-            </div>
-            <List
-                height={400}
-                itemCount={data?.length * 2}
-                itemSize={rowHeight}
-            >
+            <div className="header-row">{getHeaders()}</div>
+            <List height={400} itemCount={data?.length * 2} itemSize={rowHeight}>
                 {({ index, style }) => (
-                    <div style={style} className='table-row' >
+                    <div style={style} className="table-row">
                         {columns.map((column, columnIndex) => (
                             <div
                                 key={column?.accessorKey}
                                 className={getCellClassName(index, columnIndex)}
-                                style={{ minWidth: column?.width ?? "min-width" }}
-                                onClick={() => handleCellClickWrapper(index, columnIndex)}
-
+                                style={{ minWidth: column?.width ?? 'min-width' }}
+                                onClick={() => handleCellClick(index, columnIndex, renderCellValue(column, index))}
                             >
-                                {index % 2 === 0
-                                    ? column?.headerValue ? column.headerValue({ row: data?.[index] }) : column?.header
-                                    : column?.renderValue ? column.renderValue({ row: data?.[index] }) : data?.[index]?.[column]}
+                                {renderCellValue(column, index)}
                             </div>
                         ))}
                     </div>
                 )}
             </List>
-
         </div>
     );
 };
